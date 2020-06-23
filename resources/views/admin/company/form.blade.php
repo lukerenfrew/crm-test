8bc96d200f39ad0dcf485f82b7a1bdc3b9edf579
Luke Renfrew <luke.renfrew@mypolicy.co.uk>
2020-06-23 20:17:58 +0100

    Test validation

--- resources/views/admin/company/form.blade.php
+++ resources/views/admin/company/form.blade.php
@@ -3,24 +3,37 @@
         <label for="name" class="col-sm-2 control-label">Name</label>
         <div class="col-sm-10">
             <input type="text" class="form-control" name="name" placeholder="Name" value="{{$company->name ?? ''}}">
+            @if($errors->has('name'))
+                {{$errors->first('name')}}
+            @endif
         </div>
     </div>
     <div class="form-group">
         <label for="name" class="col-sm-2 control-label">Email</label>
         <div class="col-sm-10">
             <input type="email" class="form-control" name="email" placeholder="Email" value="{{$company->email ?? ''}}">
+            @if($errors->has('email'))
+                {{$errors->first('email')}}
+            @endif
         </div>
     </div>
     <div class="form-group">
         <label for="name" class="col-sm-2 control-label">Website</label>
         <div class="col-sm-10">
-            <input type="text" class="form-control" name="website" placeholder="Website" value="{{$company->website ?? ''}}">
+            <input type="text" class="form-control" name="website" placeholder="Website"
+                   value="{{$company->website ?? ''}}">
+            @if($errors->has('website'))
+                {{$errors->first('website')}}
+            @endif
         </div>
     </div>
     <div class="form-group">
         <label for="name" class="col-sm-2 control-label">Logo</label>
         <div class="col-sm-10">
             <input type="text" class="form-control" name="logo" placeholder="Logo" value="{{$company->logo ?? ''}}">
+            @if($errors->has('logo'))
+                {{$errors->first('logo')}}
+            @endif
         </div>
     </div>
 
--- tests/Feature/CompaniesTest.php
+++ tests/Feature/CompaniesTest.php
@@ -120,6 +120,29 @@ class CompaniesTest extends TestCase
     /**
      * @test
      */
+    public function company_is_validated_on_creation()
+    {
+        $this
+            ->actingAsAdmin()
+            ->visitRoute('company.create')
+            ->submitForm('Create', [
+                'name' => '',
+                'email' => '',
+                'logo' => '',
+                'website' => '',
+            ])
+            ->seeRouteIs('company.create')
+            ->seeText('Name field is required')
+            ->seeText('Email field is required')
+            ->seeText('Logo field is required')
+            ->seeText('Website field is required')
+            ->dontSeeText('Company created')
+            ->assertEmpty(Company::get());
+    }
+
+    /**
+     * @test
+     */
     public function can_update_company()
     {
         $company = factory(Company::class)->create([
@@ -132,10 +155,10 @@ class CompaniesTest extends TestCase
         $this
             ->actingAsAdmin()
             ->visitRoute('company.edit', $company->id)
-            ->seeInField('name','company #1')
-            ->seeInField('email','admin@company1.com')
-            ->seeInField('logo','LOGO?')
-            ->seeInField('website','http://www.company1.com')
+            ->seeInField('name', 'company #1')
+            ->seeInField('email', 'admin@company1.com')
+            ->seeInField('logo', 'LOGO?')
+            ->seeInField('website', 'http://www.company1.com')
             ->submitForm('Update', [
                 'name' => 'updated company #1',
                 'email' => 'other@company1.com',
@@ -151,6 +174,30 @@ class CompaniesTest extends TestCase
             'logo' => 'updated_LOGO?',
             'website' => 'http://www.company1.co.uk',
         ]);
+    }
+
+    /**
+     * @test
+     */
+    public function company_is_validated_on_update()
+    {
+        $company = factory(Company::class)->create();
+
+        $this
+            ->actingAsAdmin()
+            ->visitRoute('company.edit', $company->id)
+            ->submitForm('Update', [
+                'name' => '',
+                'email' => '',
+                'logo' => '',
+                'website' => '',
+            ])
+            ->seeRouteIs('company.edit', $company->id)
+            ->seeText('Name field is required')
+            ->seeText('Email field is required')
+            ->seeText('Logo field is required')
+            ->seeText('Website field is required')
+            ->dontSeeText('Company updated');
     }
 
     /**
